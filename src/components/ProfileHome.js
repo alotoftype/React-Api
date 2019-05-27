2a648113afa8f035469d2511a91ba51cc6b5e934
smjr <alotoftype@gmail.com>
2019-05-26 22:55:06 -0400

    Rendering Items
    Starting work on rendering items on the page

--- src/components/ProfileHome.js
+++ src/components/ProfileHome.js
@@ -1,17 +1,32 @@
-import React, { Component } from "react";
-import Axios from "axios";
-import FavoriteShows from "./favoriteShows";
-
-const data = Axios.get(`./data.json`);
+import React, { Component } from 'react';
+import Axios from 'axios';
+import FavoriteShows from './favoriteShows';
 
 export default class ProfileHome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shows: []
+    };
+  }
+  componentDidMount() {
+    Axios.get('https://next.json-generator.com/api/json/get/E1k5AHM6L').then(({ data }) => {
+      const shows = data
+      console.log('this is from componentDidMount', shows);
+      this.setState({ shows });
+    });
+  }
+
   render() {
+    const { shows: data } = this.state;
+    console.log('this from the class render:', data);
+
     return (
       <>
         <section>
-          <h1>Welcome to the Shows </h1>
+          <h1>Listing of Shows</h1>
         </section>
-        <section>
+        <section className='Shows'>
           <FavoriteShows shows={data} />
         </section>
       </>
--- src/components/favoriteShows.js
+++ src/components/favoriteShows.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-export const FavoriteShows = ({ shows }) => {
-  return <div>... rendered Shows</div>;
+export const FavoriteShows = ({shows}) => {
+  console.log('from inside favoriteshows',shows)
+  return (  
+    <>
+    {shows.map(show =>(
+      <div key={show.id}>
+
+      <h3>{show.title}</h3>
+      <img src={show.picture} alt=""/>
+      <p>{show.excerpt}</p>
+      </div>
+    ))}
+    </>
+  )
 };
 
 export default FavoriteShows;
